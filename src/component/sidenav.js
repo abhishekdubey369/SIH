eeac0004dd5c6ac41c0bc1ea218515c96f4bf84d
Roxrohan <rohan.shinde21@vit.edu.in>
2022-08-26 01:51:34 +0530

    fifth comment

--- src/component/sidenav.js
+++ src/component/sidenav.js
@@ -1,12 +1,50 @@
 import React from 'react';
 import './sidenav.css';
 import {Link} from 'react-router';
+import UIcon from '@mui/icons-material/UnfoldLessDouble';
+import HIcon from '@mui/icons-material/HomeWork';
+import CIcon from '@mui/icons-material/Contacts';
+import PIcon from '@mui/icons-material/PeopleAlt';
+import AIcon from '@mui/icons-material/Accessible';
+import AdIcon from '@mui/icons-material/AddLink';
+import PeIcon from '@mui/icons-material/PersonPin';
 
 const sidebar = [
     {
         item:"Home",
-        // icon:,
+        icon:<HIcon/>,
         Link:"/"
-    },]
+    },
+    {
+        item:"About",
+        icon:<UIcon/>,
+        Link:"/"
+    },
+    {
+        item:"Contact",
+        icon:<CIcon/>,
+        Link:"/"
+    },
+    {
+        item:"Add",
+        icon:<PIcon/>,
+        Link:"/"
+    },
+    {
+        item:"Help",
+        icon:<AIcon/>,
+        Link:"/"
+    },
+    {
+        item:"links",
+        icon:<AdIcon/>,
+        Link:"/"
+    },
+    {
+        item:"MyProfile",
+        icon:<PeIcon/>,
+        Link:"/"
+    },
+]
 
 // export default sidebar;
