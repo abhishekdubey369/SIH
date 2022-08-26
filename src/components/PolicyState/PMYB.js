fce592541d6b6b743c464269e0bbc7dbc433c8e8
Roxrohan <rohan.shinde21@vit.edu.in>
2022-08-26 18:09:14 +0530

    pmyb

--- src/components/PolicyState/PMYB.js
+++ src/components/PolicyState/PMYB.js
@@ -1,9 +1,95 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from "axios";
+
+//import AsyncSelect from 'react-select/async'
+//import {  FormControl, InputLabel, MenuItem, Select, TableContainer } from '@mui/material';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Container,Form,InputGroup, Table} from 'react-bootstrap'
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import { Link } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+//import { policy } from '../Data/PolicyDropDown';
+import { useStyles } from "../utils";
 
 function PMYB() {
+  const classes = useStyles();
+  const [search, setSearch] = useState('');
+  const [dataapi,setDataapi]=useState([]);
+
+  //const [PMAYStateAPI, setPMAYStateAPI] = useState([]);
+  const [selectedValue, setselectedValue] = useState([]);
+  const [navLinks, setNavLinks] = useState([]);
+
+
+  const sendRequest = async () => {
+    const res = await axios
+      .get("http://localhost:8000/api/PMYAState/getAllPMYAStates/")
+      .catch((err) => console.log(err));
+    const data = await res.data;
+    return data;
+  };
+
+
+  useEffect(() => {
+    sendRequest().then((data) => { setDataapi(data.records); console.log(data);}).catch(err => console.log(err));
+    
+  }, []);
+  console.log("fgj",dataapi);
+  
   return (
-    <div>PMYB</div>
-  )
+    
+    <div>
+      <Container>
+        <h1 className='text-center mt-4'>Pradhan Mantri fasal bima Yojna</h1>
+        <Form>
+          <InputGroup className='my-3'>
+
+            {/* onChange for search */}
+            <Form.Control
+              onChange={(e) => setSearch(e.target.value)}
+              placeholder='Search contacts'
+            />
+          </InputGroup>
+        </Form>
+        
+        <Table striped bordered hover>
+          <thead>
+            
+            <tr>
+              <th>State</th>
+              <th>Project Proposal Considered</th>
+              <th>Physical Progress Sanctioned</th>
+              <th>Physical Progress Grounded</th>
+              <th>Physical Progress Completed</th>
+              <th>Financial Progress Investment</th>
+              <th>Central Assistance Sanctioned</th>
+              <th>Central Assistance Released</th>
+            </tr>
+          </thead>
+          <tbody>
+          { dataapi.filter((item) => {
+                return search.toLowerCase() === ''
+                  ? item
+                  : item.State.toLowerCase().includes(search);
+              })
+              .map((item, index) => (
+                <tr key={index}>
+                  <td>{item.State}</td>
+                  <td>{item.ProjectProposalConsidered}</td>
+                  <td>{item.PhysicalProgressSanctioned}</td>
+                  <td>{item.PhysicalProgressGrounded}</td>
+                  <td>{item.PhysicalProgressCompleted}</td>
+                  <td>{item.FinancialProgressInvestment}</td>
+                  <td>{item.CentralAssistanceSanctioned}</td>
+                  <td>{item.CentralAssistanceReleased}</td>
+                </tr>
+              ))}
+          </tbody>
+        </Table>
+      </Container>
+    </div>
+  );
 }
 
 export default PMYB
--- src/components/SearchDistrict.js
+++ src/components/SearchDistrict.js
@@ -1,40 +1,97 @@
-import { useState } from "react";
-//import './App.css';
-// import {Data} from './'
+
+// //import './App.css';
+// // import {Data} from './'
 
 
-function SearchDistrict() {
-  // console.log(Data.state);
-  const states = ['UP', 'Delhi', 'Gujrat']
-  const cities = {
-    'UP': ['f', 'g', 'l'],
-    'Delhi': ['a', 'b'],
-    'Gujrat': ['tr', 'trt', 'rtt'],
-  }
 
-  const [selectedState, setSelectedState] = useState('')
-  console.log(selectedState)
-  return (
+// function SearchDistrict() {
+//   const [State,setState] = useState();
+//   // console.log(Data.state);
+//   // const states = states.map((data)=>{return(data.state)});
+//   // const states =["UP","Bihar","Andhra Pradesh","Arunachal Pradesh","Assam"]
+//   // const cities = {
+//   //   'UP': ['f', 'g', 'l'],
+//   //   'Delhi': ['a', 'b'],
+//   //   'Gujrat': ['tr', 'trt', 'rtt'],
+//   // }
+
+//   const [selectedState, setSelectedState] = useState('')
+//   console.log(selectedState)
+//   return (
+//     <div>
+//       {/* CASCADING DROPDOWNS : */}
+//       {/* <select onChange={(e) => { setSelectedState(e.target.value) }}>
+//         {
+//           states.map(state => {
+//             return <option>{state}</option>
+//           })
+//         }
+//         />
+
+//       {selectedState && <select>
+//         {
+//           cities[selectedState].map(city => {
+//             return <option>{city}</option>
+//           })
+//         }
+//       </select>} */}
+
+// <Select
+//    value={selectedOption}
+//    onChange={this.handleChange}
+//    options={
+//       this.props.guests.map((guest, index) => {
+//          return {
+//             label: guest,
+//             value: guest,
+//             key: index
+//          }
+//       })
+//    }
+// />
+
+//     </div>
+//   );
+// }
+
+// export default SearchDistrict;
+
+
+import React,{ useState } from "react";
+import {states} from '../Data/states-and-districts'
+
+function SDJS(){
+  const styles = {
+    backgroundColor: 'white',
+    width: '50px',
+    marginBottom: '10px',
+    padding: '10px',
+    color: 'green',
+    boxShadow: 'rgb(0,0,0,0.44) 0px 5px 5px',
+  };
+  return(
     <div>
-      CASCADING DROPDOWNS :
-      <select onChange={(e) => { setSelectedState(e.target.value) }}>
-        {
-          states.map(state => {
-            return <option>{state}</option>
-          })
-        }
+      <div>
+      <select>
+        
+        {states.map(name => (
+          <option hover={()=>(document.getElementById('id').innerHTML=(<select>
+          {name.districts.map(data =>(
+            <option>{data}</option>
+          ))}
+        </select>))}>
+          {name.state}
+          <select id="id"/>
+        </option>
+      ))}
+        
       </select>
-
-      {selectedState && <select>
-        {
-          cities[selectedState].map(city => {
-            return <option>{city}</option>
-          })
-        }
-      </select>}
-
+      {/* {states.districts.map(data =>(<li>
+        {data.map}
+      </li>))} */}
     </div>
-  );
+    </div>
+  )
 }
 
-export default SearchDistrict;
+export default SDJS;
